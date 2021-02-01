9f5bcd2463f127a2558ffa942364b43a614367a3
Emeline Sangra <emeline.sangra@gmail.com>
2021-02-01 16:50:21 +0100

    modif migration

--- migrations/Version20210201154719.php
+++ migrations/Version20210201154719.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210201112014 extends AbstractMigration
+final class Version20210201154719 extends AbstractMigration
 {
     public function getDescription() : string
     {
